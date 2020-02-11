a18dad54d1fbff6ba860db8cbc68d656d01680c1
Elton SV <el10savio96@gmail.com>
2020-02-11 10:41:07 +0530

    Converted To Package

---
+++ sieve/sieve.go
@@ -0,0 +1,41 @@
+package sieve
+
+import (
+	"math"
+)
+
+// GenerateSieve returns a list of prime numbers up to maxN
+func GenerateSieve(maxN int) []int {
+	// Create a sieve of boolean values upto maxN+1
+	maxN++
+	sieve := make([]bool, maxN)
+
+	// Since by default all empty bool values are false
+	// we will initialize all sieve elements to be true
+	// ie, all elements in the sieve are prime
+	for i := 1; i < maxN; i++ {
+		sieve[i] = true
+	}
+
+	// If a number is prime (sieve[i] = true) its corresponding
+	// multiples are marked as not prime (sieve[i] = false)
+	for i := 2; i <= int(math.Sqrt(float64(maxN))); i++ {
+		if sieve[i] == true {
+			for j := i * 2; j < maxN; j += i {
+				sieve[j] = false
+			}
+		}
+	}
+
+	// The remaining elements which are true are indeces
+	// that are prime. We will now add all indeces to the
+	// prime number list
+	primeList := make([]int, 0)
+	for i := 0; i < maxN; i++ {
+		if sieve[i] == true {
+			primeList = append(primeList, i)
+		}
+	}
+
+	return primeList
+}
